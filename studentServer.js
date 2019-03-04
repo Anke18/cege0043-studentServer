4d9cd43ba471d735848e9fdbf2fc99e8bc148ad9
Anke18 <uczlaw3@ucl.ac.uk>
2019-03-04 13:11:07 -0500

    Modify studentServer code to return test.html when it is requested

--- studentServer.js
+++ studentServer.js
@@ -9,9 +9,12 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-app.get('/',function (req,res)
+app.get('/test.html', function (req, res) 
 {
-	res.send("hello world from the HTTP server");
+	// run some server-side code
+	console.log('test.html requested');
+	// note that __dirname gives the path to the studentServer.js file
+	res.sendFile(__dirname + '/test.html');
 });
 
 // adding functionality to log the requests
