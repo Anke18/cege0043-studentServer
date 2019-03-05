f879a109f115361bfd8501af29d1da4a25f261e1
Anke18 <uczlaw3@ucl.ac.uk>
2019-03-05 09:47:55 -0500

    Connect to postgis and test

--- studentServer.js
+++ studentServer.js
@@ -9,11 +9,45 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-app.get('/:fileName', function (req, res) 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var fs = require('fs');
+var pg = require('pg');
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) 
 {
-	var fileName = req.params.fileName;
-	console.log(fileName + ' requested');
-	res.sendFile(__dirname + '/'+ fileName);
+	var split = configarray[i].split(':');
+	config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
+app.get('/postgistest', function(req,res) 
+{
+	pool.connect(function(err,client,done) 
+	{
+		if(err)
+		{
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi',
+		function(err,result)
+		{
+			done();
+			if(err)
+			{
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(result.rows);
+		});
+	});
 });
 
 app.use(function(req, res, next) 
